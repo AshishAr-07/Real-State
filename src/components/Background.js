3e7991db4447eccc0a97be8f363ab00ed5b7725a
AshishAr-07 <rawatzon@gmail.com>
2024-07-26 11:52:12 +0530

    About Section created

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,7 +4,7 @@ export default function Background() {
   return (
      <>
      {/* background image section */}
-     <div className=" bg-[url('/8074.png')] grid grid-cols-1 md:grid-cols-3 py-20 bg-fixed bg-no-repeat bg-cover bg-right  opacity-80  ">
+     <div className=" bg-[url('/8074.png')] grid grid-cols-1 md:grid-cols-3 py-20 bg-fixed bg-no-repeat bg-cover bg-right   ">
         <div className="text-center text-white pb-8 md:pb-0 "><p className="font-bold text-5xl">1548+</p><p className="text-3xl">Property Sells</p>
         </div>
         <div className="text-center text-white text-4xl pb-8 md:pb-0"><p className="font-bold text-5xl">25+</p><p className="text-3xl">Awards Gained</p>
---
+++ src/components/Hero2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Hero2() {
+  return (
+    <div className="bg-[url('/8074.png')] bg-center rounded-b-[80px] py-20">
+        <div className='flex justify-center items-center text-5xl text-white font-bold py-16'>
+            About Us
+        </div>
+    </div>
+  )
+}
