3836f86f7c10af511f4d28f7fef308b19ea65d86
AshishAr-07 <rawatzon@gmail.com>
2024-07-26 15:44:14 +0530

    Featured updated

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -3,6 +3,7 @@ import { IoStar } from "react-icons/io5";
 import { LuShrink } from "react-icons/lu";
 import { MdOutlineBathtub } from "react-icons/md";
 import { FaBed } from "react-icons/fa6";
+import { IoLocationSharp } from "react-icons/io5";
 export default function Featured() {
   return (
    
@@ -14,14 +15,19 @@ export default function Featured() {
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3 mb-20 pb-10 ">
           <div className='border-2 border-gray-100 rounded-t-3xl hover:shadow-xl'>
             <img src="2.jpg" className="rounded-t-3xl"></img>
-            <p className=" py-5 w-11/12 mx-auto" >710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
-            <div className="grid grid-cols-3 w-11/12 mx-auto">
+            <p className=" py-4 px-4 " >710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
+           <div className='grid grid-cols-8 py-4 px-4 gap-6'>
+           <p className='flex justify-start items-center '><IoLocationSharp size={25} /></p>
+          <p className='col-span-7 font-light text-sm'>710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
+           </div>
+           
+            <div className="grid grid-cols-3 px-4 py-4 ">
               <div className="flex justify-start items-center "><LuShrink size={25} /><p className="pl-2">8000 sqf</p> </div>
               <div className="flex justify-start items-center"><FaBed size={25} /> <p className="pl-2">4 Beds</p> </div>
               <div className="flex justify-start items-center "><MdOutlineBathtub size={25} /><p className="pl-2">4 Baths</p> </div>
 
             </div>
-            <div className="grid grid-cols-2 mb-5 py-4 w-11/12 mx-auto">
+            <div className="grid grid-cols-2 mb-5 py-4 px-4">
               <div>
                 <div>Price</div>
                 <div>$450</div>
@@ -30,29 +36,33 @@ export default function Featured() {
                 <p>
                   Rating
                 </p>
-                <div className="grid grid-cols-7">
+                <div className="grid grid-cols-5">
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
-                  <p>5.0</p>
-                  <p>(30)</p>
+                  
                 </div>
               </div>
             </div>
 
           </div>
-          <div className='border-2 border-gray-100 rounded-t-3xl  hover:shadow-xl'>
+          <div className='border-2 border-gray-100 rounded-t-3xl hover:shadow-xl'>
             <img src="2.jpg" className="rounded-t-3xl"></img>
-            <p className=" py-5 w-11/12 mx-auto" >710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
-            <div className="grid grid-cols-3 w-11/12 mx-auto">
+            <p className=" py-4 px-4 " >710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
+           <div className='grid grid-cols-8 py-4 px-4 gap-6'>
+           <p className='flex justify-start items-center '><IoLocationSharp size={25} /></p>
+          <p className='col-span-7 font-light text-sm'>710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
+           </div>
+           
+            <div className="grid grid-cols-3 px-4 py-4 ">
               <div className="flex justify-start items-center "><LuShrink size={25} /><p className="pl-2">8000 sqf</p> </div>
               <div className="flex justify-start items-center"><FaBed size={25} /> <p className="pl-2">4 Beds</p> </div>
               <div className="flex justify-start items-center "><MdOutlineBathtub size={25} /><p className="pl-2">4 Baths</p> </div>
 
             </div>
-            <div className="grid grid-cols-2 mb-5 py-4 w-11/12 mx-auto">
+            <div className="grid grid-cols-2 mb-5 py-4 px-4">
               <div>
                 <div>Price</div>
                 <div>$450</div>
@@ -61,29 +71,33 @@ export default function Featured() {
                 <p>
                   Rating
                 </p>
-                <div className="grid grid-cols-7">
+                <div className="grid grid-cols-5">
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
-                  <p>5.0</p>
-                  <p>(30)</p>
+                  
                 </div>
               </div>
             </div>
 
           </div>
-          <div className='border-2 border-gray-100 rounded-t-3xl  hover:shadow-xl '>
+          <div className='border-2 border-gray-100 rounded-t-3xl hover:shadow-xl'>
             <img src="2.jpg" className="rounded-t-3xl"></img>
-            <p className=" py-5 w-11/12 mx-auto" >710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
-            <div className="grid grid-cols-3 w-11/12 mx-auto">
+            <p className=" py-4 px-4 " >710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
+           <div className='grid grid-cols-8 py-4 px-4 gap-6'>
+           <p className='flex justify-start items-center '><IoLocationSharp size={25} /></p>
+          <p className='col-span-7 font-light text-sm'>710 BOYD DR, Unit #1102, Baton Rouge, LA 70808, USA</p>
+           </div>
+           
+            <div className="grid grid-cols-3 px-4 py-4 ">
               <div className="flex justify-start items-center "><LuShrink size={25} /><p className="pl-2">8000 sqf</p> </div>
               <div className="flex justify-start items-center"><FaBed size={25} /> <p className="pl-2">4 Beds</p> </div>
               <div className="flex justify-start items-center "><MdOutlineBathtub size={25} /><p className="pl-2">4 Baths</p> </div>
 
             </div>
-            <div className="grid grid-cols-2 mb-5 py-4 w-11/12 mx-auto">
+            <div className="grid grid-cols-2 mb-5 py-4 px-4">
               <div>
                 <div>Price</div>
                 <div>$450</div>
@@ -92,14 +106,13 @@ export default function Featured() {
                 <p>
                   Rating
                 </p>
-                <div className="grid grid-cols-7">
+                <div className="grid grid-cols-5">
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
                   <p><IoStar color="#febf1c" size={22} /></p>
-                  <p>5.0</p>
-                  <p>(30)</p>
+                  
                 </div>
               </div>
             </div>
